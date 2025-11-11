4ba6535c0ac414f3ee79aed7a43621dfa96c7b82
cezarasilva <cezar.antonio.silva@gmail.com>
2025-11-10 22:02:19 -0300

    aplicando responsividade home

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,6 @@
 // assets/js/header.js
 
+// ===== Efeito de scroll no header =====
 const header = document.querySelector('.header');
 
 window.addEventListener('scroll', () => {
@@ -8,4 +9,24 @@ window.addEventListener('scroll', () => {
   } else {
     header.classList.remove('scrolled');
   }
+});
+
+// ===== Menu hambúrguer =====
+const menuIcon = document.querySelector('#menu-icon');
+const menu = document.querySelector('.header-menu');
+
+
+// Alternar visibilidade do menu
+menuIcon.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
+
+// Fechar o menu ao clicar em um link (opcional, mas elegante)
+const menuLinks = document.querySelectorAll('.header-menu a');
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
 });
